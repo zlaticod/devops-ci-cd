65f88dca46d4ec796febc719b20d318acb3711e2
Ivo Gergov <6384922+igergow@users.noreply.github.com>
2025-10-14 12:24:51 +0300

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mydomain.com",
-  base: "/",
+  site: "https://tu-sofia-lessons.github.io",
+  base: "/devops-ci-cd",
   trailingSlash: "ignore", // Поддържа URL-и със и без trailing slash
 });
